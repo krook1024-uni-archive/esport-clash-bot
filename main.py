b8fdc5190480ab440975f725b06f7c4089ea5d61
Molnár Antal Albert <krook1024@gmail.com>
2020-05-04 10:28:53 +0200

    discord bot's basic functionality

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ auth = [x.strip() for x in auth]
 
 bot = commands.Bot(command_prefix="!")
 
-channel_id = int(auth[1])
+CHANNEL_ID = int(auth[1])
 
 @bot.command()
 async def szia(ctx):
     await ctx.send("Szia!")
+
+bot.run(auth[0])
